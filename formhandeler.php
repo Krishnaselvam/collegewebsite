f60453439b08d987c1fb237698a054d60e7853e3
krishnadeveloper <89072286+krishna-webdev@users.noreply.github.com>
2021-11-06 14:10:39 +0545

    Add files via upload

---
+++ formhandeler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'pkrishnavdr@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_bodt = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $visitor_email.\n";
+
+$to = 'pkrishnavdr@gmail.com' ;
+
+$headers = "From $email_from \r\n;"              
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers)
+
+header("Location: contact.html");
+
+
+
+
+
+?>
